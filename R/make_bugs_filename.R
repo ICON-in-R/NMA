95226efd36d1739299b91f6192165a44f95da444
Dr Nathan Green <n8thangreen@yahoo.co.uk>
2023-11-07 14:57:29 +0000

    trying fs::path_package() so that can access inst folder file outside of devtools dev

--- R/make_bugs_filename.R
+++ R/make_bugs_filename.R
@@ -13,6 +13,8 @@ make_bugs_filename <- function(random, data_type) {
   data_name <- gsub(pattern = "_data", replacement = "", data_type)
   fname <- c(effect, data_name)
   
-  here::here("inst", paste0(paste(fname, collapse = "_"), ".txt"))
+  file_name <- paste0(paste(fname, collapse = "_"), ".txt")
+  fs::path_package(file_name, package = "NMA")
+  # here::here("inst", file_name)
 }
 
